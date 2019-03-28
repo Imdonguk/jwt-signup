0977973c0b8070f996679597622a7ba3f56a23ca
Imdonguk <com8157@naver.com>
2019-03-28 16:34:32 +0900

    Add : localStorage의 토큰값이 있을때 메인페이지 없을 땐 signin라우트로 이동 뼈대코드만 작성

---
+++ client/src/routes/RootPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { Main } from 'routes'
+
+const RootPage = () => {
+    const isToken = !!localStorage.getItem('token')
+    return (
+        <div>
+            {!isToken && <Redirect to='signin' from='/' />}
+            <Main />
+        </div>
+    );
+};
+
+export default RootPage;

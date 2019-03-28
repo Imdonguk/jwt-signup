64c07a37b7de379e0a64674807a98ada4f2571aa
Imdonguk <com8157@naver.com>
2019-03-28 16:31:38 +0900

    Add : 라우트 설정 뼈대코드작성

---
+++ client/src/routes/index.js
@@ -0,0 +1,5 @@
+export { default as Root } from './RootPage';
+export { default as Main } from './MainPage';
+export { default as Signin } from './SigninPage';
+export { default as Signup } from './SignupPage';
+export { default as NotFound } from './NotFound';
--- client/src/shared/App.js
+++ client/src/shared/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { Root, Signin, Signup, NotFound } from 'routes';
 
 const App = () => {
     return (
         <div>
-
+            <Switch>
+                <Route exact path='/' component={Root}></Route>
+                <Route path='/signin' component={Signin}></Route>
+                <Route path='/signup' component={Signup}></Route>
+                <Route component={NotFound}></Route>
+            </Switch>
         </div>
     );
 };

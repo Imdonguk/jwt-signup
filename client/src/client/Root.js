1ef708b4f2678824e389bbb4e6e2600a20087b7e
Imdonguk <com8157@naver.com>
2019-03-28 15:23:56 +0900

    Init : 기본설정 디렉토리 구조잡기

---
+++ client/src/client/Root.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from 'shared/App'
+const Root = () => {
+    return (
+        <Router>
+            <App />
+        </Router>
+    );
+};
+
+export default Root;

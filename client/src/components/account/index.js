635275e2d681aaa02eab5a4cbc0b5650d96452c2
Imdonguk <com8157@naver.com>
2019-04-06 17:32:51 +0900

    Add : 해당 컴포넌트에 쉽게 접근할 때 폴더명을 통해서 접근하기 위해서 컴포넌트 폴더마다 index.js파일추가

---
+++ client/src/components/account/index.js
@@ -0,0 +1,5 @@
+export { default as AppDown } from './AppDown';
+export { default as Input } from './Input';
+export { default as LetterIcon } from './LetterIcon';
+export { default as Button } from './Button';
+export { default as HiperLink } from './HiperLink';

7e539615ff05deb92c5fb31d776b448feaab0b73
Imdonguk <com8157@naver.com>
2019-04-11 20:55:27 +0900

    Modify : /users로 요청시 respose객체로 user계정정보전달

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var user = require('../model/userSchema.js')
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async (req, res, next) => {
+  const accountsInfo = await user.find()
+  res.json(accountsInfo);
 });
 
 module.exports = router;

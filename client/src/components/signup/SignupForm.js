c85ec4df44d776009264bcacfe47657dbc9a083a
Imdonguk <com8157@naver.com>
2019-04-12 18:32:51 +0900

    Test : fetch api를 사용하여 signup라우터 post방식 테스트

--- client/src/components/signup/SignupForm.js
+++ client/src/components/signup/SignupForm.js
@@ -6,6 +6,19 @@ const SignupForm = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        fetch('/signup', {
+            method: 'post',
+            body: JSON.stringify({
+                "woogie": "post api test"
+            }),
+            headers: {
+                'content-type': 'application/json'
+            },
+            mode: 'cors'
+        })
+            .then(res => res.json())
+            .then(json => console.log(json))
+            .catch(e => console.log(e));
     }
 
     return (
@@ -14,9 +27,9 @@ const SignupForm = () => {
                 <LetterIcon />
                 <h2 className='content-form-title'>친구들의 사진과 동영상을 보려면 가입하세요.</h2>
                 <form className='content-form' onSubmit={handleSubmit}>
-                    <input className='content-form-input' type='text' placeholder='성명' />
-                    <input className='content-form-input' type='text' placeholder='사용자 이름' />
-                    <input className='content-form-input' type='password' placeholder='비밀번호' />
+                    <input className='content-form-input' name='name' type='text' placeholder='성명' />
+                    <input className='content-form-input' name='userName' type='text' placeholder='사용자 이름' />
+                    <input className='content-form-input' name='password' type='password' placeholder='비밀번호' />
                     <button className='content-form-button' type='submit'>가입</button>
                 </form>
                 <p className='content-form-policy'>

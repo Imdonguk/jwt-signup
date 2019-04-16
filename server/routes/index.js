759be356f19650d32079b84f03d7b7ec5ef2ddb8
Imdonguk <com8157@naver.com>
2019-04-16 14:12:59 +0900

    Add : api요청시 응답, 기존 디비에 데이터가 없을 시에 디비에 이름, 사용자이름, 비밀번호 저장

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require('../model/userSchema.js')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -7,7 +8,24 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/signup', (req, res) => {
-  console.log(req.body);
-  res.json({ success: 'ok' })
+  const { name, userName, password } = req.body;
+  if (!name) return res.send({ success: false, msg: '이름이 없습니다.' })
+  if (!userName) return res.send({ success: false, msg: '사용자 이름이 없습니다.' })
+  if (!password) return res.send({ success: false, msg: '비밀번호가 없습니다.' })
+
+  User.findOne({ userName })
+    .then(r => {
+      if (r) throw new Error('기존에 있는 사용자이름입니다.')
+      User.create({
+        name,
+        userName,
+        password
+      })
+      res.send({ success: true })
+    })
+    .catch(e => {
+      res.send({ success: false, msg: e.message })
+    })
+})
 })
 module.exports = router;

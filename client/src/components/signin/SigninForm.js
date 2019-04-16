2dc1c52ae6e60ae8894e541390ad955af3639aee
Imdonguk <com8157@naver.com>
2019-04-16 14:22:53 +0900

    Add : account상태값 선언 및 이벤트핸들러함수인 handleChange동작시 account상태값 변경

--- client/src/components/signin/SigninForm.js
+++ client/src/components/signin/SigninForm.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SigninForm.scss';
 import { LetterIcon, AppDown, HiperLink } from '../account';
 const SigninForm = () => {
+
+    const [account, setAccount] = useState({});
     const handleSubmit = (e) => {
         e.preventDefault();
+
+    const handleChange = ({ target }) => {
+        setAccount({
+            ...account,
+            [target.name]: target.value
+        })
     }
     return (
         <div className='content'>
             <div className='content-box'>
                 <LetterIcon />
                 <form className='content-form' onSubmit={handleSubmit}>
-                    <input className='content-form-input' type='text' placeholder='사용자이름' />
-                    <input className='content-form-input' type='password' placeholder='패스워드' />
-                    <button className='content-form-button' type='submit' value='로그인'>로그인</button>
+                    <input
+                        className='content-form-input'
+                        type='text'
+                        name='userName'
+                        placeholder='사용자이름'
+                        onChange={handleChange}
+                    />
+                    <input
+                        className='content-form-input'
+                        type='password'
+                        name='password'
+                        placeholder='패스워드'
+                        onChange={handleChange}
+                    />
+                    <button className='content-form-button' type='submit'>로그인</button>
                 </form>
             </div>
             <div className='content-box'>

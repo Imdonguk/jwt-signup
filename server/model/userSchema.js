fdb3848e1fa396683f868ecbe9d36a87e9802c7b
Imdonguk <com8157@naver.com>
2019-04-18 20:47:01 +0900

    Modify : user 스키마 설정 수정(unique,index 등)

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,10 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+mongoose.set('useCreateIndex', true);
 const userSchema = new Schema({
-    name: { type: String, default: '' },
-    userName: { type: String, default: '' },
-    password: { type: String, default: '' }
+    name: {
+        type: String,
+        required: true
+    },
+    userName: {
+        type: String,
+        unique: true,
+        index: true,
+        required: true
+    },
+    pwd: {
+        type: String,
+        required: true
+    }
 })
 
 module.exports = mongoose.model('user', userSchema);

90827db7f3996059c907517394f7e172d0542b89
Imdonguk <com8157@naver.com>
2019-04-19 00:53:54 +0900

    Add : nodejs 제공 api crypto를 사용하여 비밀번호 암호화하는 encode함수 선언

--- server/routes/account/index.js
+++ server/routes/account/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const crypto = require('crypto');
 
 router.get('/', (req, res) => {
     res.send({ 'account': 'welcome' })
 })
+
+
+function encode(pwd, salt) {
+    return crypto.scryptSync(pwd, salt, 64, { N: 1024 }).toString('hex');
+}
 
 module.exports = router;

f0868340a0fe4e6752ca70858f050f8808cc0fb7
Imdonguk <com8157@naver.com>
2019-04-19 12:42:53 +0900

    Modify : Footer, AppDown 컴포넌트 태그 및 스타일수정

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,17 +7,17 @@ const Footer = () => {
             <div className='footer-inner'>
                 <nav className='footer-inner-link'>
                     <ul>
-                        <li><a href='#'>instagram 정보</a></li>
-                        <li><a href='#'>지원</a></li>
-                        <li><a href='#'>홍보 센터</a></li>
-                        <li><a href='#'>API</a></li>
-                        <li><a href='#'>채용 정보</a></li>
-                        <li><a href='#'>개인정보처리방침</a></li>
-                        <li><a href='#'>약관</a></li>
-                        <li><a href='#'>디렉터리</a></li>
-                        <li><a href='#'>프로필</a></li>
-                        <li><a href='#'>해시태그</a></li>
-                        <li><a href='#'>언어</a></li>
+                        <li><span className='link'>instagram 정보</span></li>
+                        <li><span className='link'>지원</span></li>
+                        <li><span className='link'>홍보 센터</span></li>
+                        <li><span className='link'>API</span></li>
+                        <li><span className='link'>채용 정보</span></li>
+                        <li><span className='link'>개인정보처리방침</span></li>
+                        <li><span className='link'>약관</span></li>
+                        <li><span className='link'>디렉터리</span></li>
+                        <li><span className='link'>프로필</span></li>
+                        <li><span className='link'>해시태그</span></li>
+                        <li><span className='link'>언어</span></li>
                     </ul>
                 </nav>
                 <span>© 2019 Instagram</span>
--- client/src/components/account/AppDown.js
+++ client/src/components/account/AppDown.js
@@ -5,11 +5,17 @@ const AppDown = () => {
         <div className='download'>
             <p>앱을 다운로드하세요.</p>
             <div className='app'>
-                <a href='#'>
-                    <img src='https://www.instagram.com/static/images/appstore-install-badges/badge_ios_korean-ko.png/4a5c9d62d51b.png' />
+                <a href='https://itunes.apple.com/app/instagram/id389801252?mt=8&vt=lo'>
+                    <img
+                        src='https://www.instagram.com/static/images/appstore-install-badges/badge_ios_korean-ko.png/4a5c9d62d51b.png'
+                        alt='download apple app'
+                    />
                 </a>
-                <a href='#'>
-                    <img src='https://www.instagram.com/static/images/appstore-install-badges/badge_android_korean-ko.png/f155b664a93b.png' />
+                <a href='https://play.google.com/store/apps/details?id=com.instagram.android&referrer=utm_source%3Dinstagramweb%26utm_campaign%3DsignupPage%26ig_mid%3DW2g8NgAEAAH6Rs-vxib9xZKDIR8h%26utm_content%3Dlo%26utm_medium%3Dbadge'>
+                    <img
+                        src='https://www.instagram.com/static/images/appstore-install-badges/badge_android_korean-ko.png/f155b664a93b.png'
+                        alt='download google app'
+                    />
                 </a>
             </div>
         </div>
